84e47217a8be78951e558b17c0dfcd2d0b82e1fb
Mateus Pragana <mateuspragana@gmail.com>
2022-12-18 08:18:15 -0300

    Finalizando aula 03: Infraestrutura e injeção de dependências

---
+++ src/application/use-cases/unred-notification.spec.ts
@@ -0,0 +1,34 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationRepository } from '@test/repositories/in-memory-notifications-repository';
+import { NotificationNotFound } from './errors/notification-not-found';
+import { UnreadNotification } from './unread-notification';
+
+describe('Unread notification', () => {
+  it('should be able to unread a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    const notification = makeNotification({
+      readAt: new Date(),
+    });
+
+    await notificationsRepository.create(notification);
+
+    await unreadNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].readtAt).toBeNull();
+  });
+
+  it('should not be able to unread a notification when it does not exist', async () => {
+    const notificationsRepository = new InMemoryNotificationRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    expect(async () => {
+      return await unreadNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
